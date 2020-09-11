80e742b39c9e64d4dacb6934e1ebfe294b7dbb11
Dustin Arendt <dustin.arendt@pnnl.gov>
2020-09-10 20:18:17 -0700

    Compute the props inside the python class for the widget.

--- widget/hnxwidget/hypernetx_widget.py
+++ widget/hnxwidget/hypernetx_widget.py
@@ -2,7 +2,57 @@ from .react_jupyter_widget import ReactJupyterWidget
 
 import ipywidgets as widgets
 
+from hypernetx.drawing.util import get_set_layering
+
 @widgets.register
 class HypernetxWidget(ReactJupyterWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, H,
+        node_size=None,
+        **kwargs
+    ):
+        # will break if already collapsed
+        self.H = H.collapse_nodes_and_edges()
+
+        def get_node_size(v):
+            if node_size is None:
+                return 1
+            elif hasattr(node_size, 'get'):
+                return node_size.get(v, 1)
+            else:
+                return node_size
+        
+        levels = get_set_layering(self.H)
+        
+        nodes = [
+            {
+                'elements': [
+                    {
+                        'uid': v,
+                        'value': get_node_size(v)
+                    }
+                    for v in entity.uid
+                ]
+            }
+            for i, entity in enumerate(self.H.nodes())
+        ]
+
+        nodes_dict = {
+            entity: i
+            for i, entity in enumerate(self.H)
+        }
+                
+        # js friendly representation of the hypergraph
+        edges = [
+            {
+                'uid': list(entity.uid),
+                'elements': [nodes_dict[v] for v in entity.elements],
+                'level': levels[entity.uid]
+            }
+            for entity in self.H.edges()
+        ]
+        
+        super().__init__(
+            nodes=nodes,
+            edges=edges,
+            **kwargs
+        )
