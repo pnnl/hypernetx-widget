90b7c04fcb4e8e1281bdb1dbeb53730748e45520
Dustin Arendt <dustin.arendt@pnnl.gov>
2020-11-15 20:41:54 -0800

    Add a grid layout to structure the overall app, with a left side allocated for controls, right side for the main visualization.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
 import {HypernetxWidgetView} from './HypernetxWidgetView'
 
 export const HypernetxWidget = props =>
-  <HypernetxWidgetView {...props} /> 
+  <Grid container spacing={1}>
+    <Grid item xs={12} sm={2}>
+      <Paper>
+        Controls
+      </Paper>
+    </Grid>
+
+    <Grid item xs={12} sm={10}>
+      <Paper>
+        <HypernetxWidgetView {...props} /> 
+      </Paper>
+    </Grid>
+
+  </Grid>
 
 export default HypernetxWidget
