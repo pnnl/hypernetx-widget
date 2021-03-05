9f67c421b26bc4924416d1ffc228a6aadda67680
Dustin Arendt <dustin.arendt@pnnl.gov>
2021-03-05 15:10:23 -0800

    Add stories for node/edge selection, hiding, and removal.

--- src/stories/hnx-widget-encodings.stories.js
+++ src/stories/hnx-widget-encodings.stories.js
@@ -12,5 +12,24 @@ export default {
 
 // console.log(props)
 
-export const Radius = () =>
-  <HypernetxWidgetView {...props} />
+const nodes = {'JV': true, 'TH': true};
+const edges = {'1': true, '2': true};
+
+export const SelectedNodes = () =>
+  <HypernetxWidgetView {...props} selectedNodes={nodes} />
+
+export const SelectedEdges = () =>
+  <HypernetxWidgetView {...props} selectedEdges={edges} />
+
+export const HiddenNodes = () =>
+  <HypernetxWidgetView {...props} hiddenNodes={nodes}  />
+
+export const HiddenEdges = () =>
+  <HypernetxWidgetView {...props} hiddenEdges={edges}  />
+
+export const RemovedNodes = () =>
+  <HypernetxWidgetView {...props} removedNodes={nodes}  />
+
+export const RemovedEdges = () =>
+  <HypernetxWidgetView {...props} removedEdges={edges}  />
+
