99f29a3e3dffdd40d628951658f1c08f71c19e82
Dustin Arendt <dustin.arendt@pnnl.gov>
2021-05-12 18:35:32 -0700

    Fixed spelling mistake, updated github URL in setup.py

--- widget/setup.py
+++ widget/setup.py
@@ -23,7 +23,7 @@ log.info('setup.py entered')
 log.info('$PATH=%s' % os.environ['PATH'])
 
 name = 'hnxwidget'
-LONG_DESCRIPTION = 'A widget for interractive visualization of the hypernetx package.'
+LONG_DESCRIPTION = 'A widget for interactive visualization of the hypernetx package.'
 
 
 def js_prerelease(command, strict=False):
@@ -152,7 +152,7 @@ setup_args = dict(
     },
     author='Dustin Arendt',
     author_email='dustin.arendt@pnnl.gov',
-    url='https://github.com/PNNL/hnx-widget',
+    url='https://github.com/pnnl/hypernetx-widget',
     keywords=[
         'ipython',
         'jupyter',
