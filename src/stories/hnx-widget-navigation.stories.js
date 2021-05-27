b2373750aa4dedfe03c499738e063cbaf43d388a
Dustin Arendt <dustin.arendt@pnnl.gov>
2021-05-27 16:00:07 -0700

    Added a simple story to test all 5 interaction modes.

--- src/stories/hnx-widget-navigation.stories.js
+++ src/stories/hnx-widget-navigation.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import NavigableSVG, {PAN, ZOOM_IN, ZOOM_OUT}  from '../NavigableSVG'
+import NavigableSVG, {PAN, RESET, ZOOM_IN, ZOOM_OUT}  from '../NavigableSVG'
 
 export default {
   title: 'HNX Widget SVG/Navigation',
@@ -17,6 +17,31 @@ const Pattern = () => <React.Fragment>
   <circle cx={20} cy={80} r={16} />
   <text x={150} y={150}>How do we make font size constant?</text>
 </React.Fragment>
+
+function SimpleNavigation() {
+  const [nav, setNav] = React.useState();
+
+  const options = [undefined, RESET, PAN, ZOOM_IN, ZOOM_OUT];
+
+  const handleClick = ev =>
+    console.log(ev.target.value) ||
+    setNav(ev.target.value)
+
+  return <div>
+    { options.map(d =>
+        <div>
+          <button value={d} onClick={handleClick}>
+            {d || 'Undefined'}
+          </button>
+        </div>
+      )
+    }
+
+    <NavigableSVG {...props} navigation={nav}>
+      <Pattern />
+    </NavigableSVG>
+  </div>
+}
 
 export const Default = () =>
   <NavigableSVG {...props} >
@@ -37,3 +62,6 @@ export const ZoomOut = () =>
   <NavigableSVG {...props} navigation={ZOOM_OUT}  >
     <Pattern />
   </NavigableSVG>
+
+export const Interactive = () =>
+  <SimpleNavigation />
