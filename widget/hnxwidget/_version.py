993f396ff2f1909b21ecb7fde8147e50520e63b8
Dustin Arendt <dustin.arendt@pnnl.gov>
2021-09-30 15:19:10 -0700

    Increment version

--- widget/hnxwidget/_version.py
+++ widget/hnxwidget/_version.py
@@ -1,5 +1,6 @@
 # Module version
-version_info = (0, 1, 0, 'beta', 5)
+
+version_info = (0, 1, 1, 'beta', 0)
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
